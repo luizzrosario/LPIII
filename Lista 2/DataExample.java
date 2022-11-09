5d195d9a55b0fc4b07d6173f02760a02c4b7d6bb
luizzrosario <luizfeliper57@gmail.com>
2022-11-09 19:20:53 -0300

    data corrigido

--- Lista 2/DataExample.java
+++ Lista 2/DataExample.java
@@ -1,14 +1,17 @@
 public class DataExample {
     public static void main(String[] args) {
         Data data1 = new Data();
+        String dataTemp;
 
         data1.setData(22, 9, 2003);
 
-        data1.imprimirData();
+        dataTemp = data1.imprimirData();
 
-        System.out.println();
+        System.out.println(dataTemp);
 
-        data1.imprimirDataS();
+        dataTemp = data1.imprimirDataS();
+
+        System.out.println(dataTemp);
 
     }
 }
